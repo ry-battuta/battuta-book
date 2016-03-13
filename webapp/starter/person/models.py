d51992151acd052caa2a4a672e6c9fa24a3727eb
Tritico <m.tritico@gmail.com>
2016-03-13 15:23:15 -0300

    adding questions and tags

--- webapp/starter/person/models.py
+++ webapp/starter/person/models.py
@@ -4,6 +4,26 @@ from rest_framework.authtoken.models import Token
 from django.db import models
 from django.db.models.signals import post_save, pre_save
 
+
+class Tag(models.Model):
+
+    class Meta:
+        db_table = 'tag'
+
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    name = models.CharField(max_length=40, blank=True)
+    group = models.CharField(max_length=40, blank=True)
+
+
+class Question(models.Model):
+
+    class Meta:
+        db_table = 'question'
+
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    name = models.CharField(max_length=40, blank=True)
+
+
 class Person(models.Model):
 
     class Meta:
@@ -21,23 +41,32 @@ class Person(models.Model):
 
     first_name = models.CharField(max_length=40, blank=True)
     last_name = models.CharField(max_length=40, blank=True)
-    twitter_handle = models.CharField(max_length=40, blank=True)
+
     homebase = models.CharField(max_length=40, blank=True)
     profession = models.CharField(max_length=40, blank=True)
     current_location = models.CharField(max_length=40, blank=True)
     blurb = models.TextField(max_length=200, blank=True)
 
-    #avatar = models.CharField(max_length=40, blank=True)
-    #passions = LIST
-    #skills = LIST
+    ry_gen_name = models.CharField(max_length=40, blank=True)
+    ry_gen_date = models.DateField(null=True, blank=True)
+
+    social_website = models.CharField(max_length=255, blank=True)
+    social_facebook = models.CharField(max_length=255, blank=True)
+    social_instagram = models.CharField(max_length=255, blank=True)
+    social_twitter = models.CharField(max_length=255, blank=True)
+    social_linkedin = models.CharField(max_length=255, blank=True)
+    social_medium = models.CharField(max_length=255, blank=True)
+    social_podcast = models.CharField(max_length=255, blank=True)
+    social_other = models.CharField(max_length=255, blank=True)
+
+    # relationships
+    tags = models.ManyToManyField(Tag)
+    questions = models.ManyToManyField(Question)
 
     def get_full_name(self):
         # The user is identified by their email address
-        return self.email
+        return self.first_name + " " + self.last_name
 
-    def get_short_name(self):
-        # The user is identified by their email address
-        return self.email
 
     def get_avatar(self):
         email = self.email
@@ -60,8 +89,13 @@ class Person(models.Model):
 def ensure_lower_case_email(instance, sender, **kwargs):
     instance.email = instance.email.lower()
 
+
+def ensure_lower_case_name(instance, sender, **kwargs):
+    instance.name = instance.name.lower()
 # SIGNALS :::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
 
 # Attach signals to User model
-#pre_save.connect(ensure_lower_case_email, sender=Person)
+pre_save.connect(ensure_lower_case_email, sender=Person)
+pre_save.connect(ensure_lower_case_name, sender=Tag)
+pre_save.connect(ensure_lower_case_name, sender=Question)
 #post_save.connect(generate_token_on_creation, sender=Person)
