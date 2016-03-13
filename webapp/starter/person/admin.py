d04aedba04037d640a54bf93eda080b84c3c3586
Tritico <m.tritico@gmail.com>
2016-03-13 15:23:15 -0300

    adding questions and tags

--- webapp/starter/person/admin.py
+++ webapp/starter/person/admin.py
@@ -1,11 +1,12 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import Person
+from .models import Person, Tag, Question
+
 
 @admin.register(Person)
 class PersonAdmin(admin.ModelAdmin):
-    list_display = ('id', 'email', 'first_name', 'last_name',)
+    list_display = ('email', 'first_name', 'last_name',)
     ordering = ('id',)
     search_fields = ('name', 'id')
     readonly_fields = ('id',)
@@ -22,4 +23,38 @@ class PersonAdmin(admin.ModelAdmin):
             fieldsets = [(None, {'fields': ('first_name', 'last_name')}),
                          (None, {'fields': ('twitter_handle', 'homebase', 'profession', 'current_location')}),
                          ('Blurb', {'fields': ('blurb',)}), ]
+            return fieldsets
+
+
+@admin.register(Tag)
+class TagAdmin(admin.ModelAdmin):
+    list_display = ('name',)
+    ordering = ('name',)
+    search_fields = ('name',)
+
+    def get_fieldsets(self, request, obj=None):
+        if obj:
+            # update
+            fieldsets = [(None, {'fields': ('id', 'name')}),]
+            return fieldsets
+        else:
+            # create
+            fieldsets = [(None, {'fields': ('id', 'name')}),]
+            return fieldsets
+
+
+@admin.register(Question)
+class QuestionAdmin(admin.ModelAdmin):
+    list_display = ('name',)
+    ordering = ('name',)
+    search_fields = ('name',)
+
+    def get_fieldsets(self, request, obj=None):
+        if obj:
+            # update
+            fieldsets = [(None, {'fields': ('id', 'name')}),]
+            return fieldsets
+        else:
+            # create
+            fieldsets = [(None, {'fields': ('id', 'name')}),]
             return fieldsets
